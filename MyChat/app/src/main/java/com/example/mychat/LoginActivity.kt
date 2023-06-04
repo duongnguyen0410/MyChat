da1f07f022759225a4fb261472e24e138fad411f
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-06-04 15:02:07 +0700

    first Google login code

--- MyChat/app/src/main/java/com/example/mychat/LoginActivity.kt
+++ MyChat/app/src/main/java/com/example/mychat/LoginActivity.kt
@@ -1,11 +1,85 @@
 package com.example.mychat
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.mychat.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val btGGLogin: SignInButton = binding.btGGLogin
+
+        val googleTextView: TextView = btGGLogin.getChildAt(0) as TextView
+        googleTextView.text = "Log in wigh Google"
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        btGGLogin.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        laucher.launch(signInIntent)
+    }
+
+    private val laucher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account: GoogleSignInAccount? = task.result
+            if(account != null){
+                updateUI(account)
+            }
+        }else{
+            Log.e("GoogleSignIn", task.exception.toString())
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if(it.isSuccessful){
+                Toast.makeText(this, "loginsuccess", Toast.LENGTH_SHORT).show()
+            }else{
+                Log.e("GoogleSignIn", it.exception.toString())
+            }
+        }
     }
 }
