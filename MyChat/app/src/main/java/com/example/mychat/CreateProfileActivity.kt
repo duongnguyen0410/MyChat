b41e9dc56c7ef185fe51e8cf50683414343db751
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-06-08 18:16:01 +0700

    add select image from device's storage function

--- MyChat/app/src/main/java/com/example/mychat/CreateProfileActivity.kt
+++ MyChat/app/src/main/java/com/example/mychat/CreateProfileActivity.kt
@@ -1,11 +1,48 @@
 package com.example.mychat
 
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.mychat.databinding.ActivityCreateProfileBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import de.hdodenhof.circleimageview.CircleImageView
 
 class CreateProfileActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 1
+    private val PERMISSION_REQUEST_CODE = 2
+
+    private lateinit var imageView: ImageView
+    private lateinit var selectedImageUri: Uri
+    private lateinit var storageReference: StorageReference
+
+    private lateinit var binding: ActivityCreateProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_profile)
+
+        binding = ActivityCreateProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val ivProfile: CircleImageView = binding.ivProfile
+
+        val storage = Firebase.storage
+        storageReference = storage.reference
+    }
+
+    fun onImageViewClick(view: View) {
+
+    }
+
+    private fun openImagePicker() {
+
     }
 }
