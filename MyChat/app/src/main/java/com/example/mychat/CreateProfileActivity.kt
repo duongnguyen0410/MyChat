3d9ce1abdcef11d35c8ed91382d633ac3821ae3c
Duong Nguyen <20521220@gm.uit.edu.vn>
2023-06-09 15:15:56 +0700

    add function code

--- MyChat/app/src/main/java/com/example/mychat/CreateProfileActivity.kt
+++ MyChat/app/src/main/java/com/example/mychat/CreateProfileActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mychat
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.mychat.databinding.ActivityCreateProfileBinding
@@ -19,8 +22,8 @@ class CreateProfileActivity : AppCompatActivity() {
     private val PICK_IMAGE_REQUEST = 1
     private val PERMISSION_REQUEST_CODE = 2
 
-    private lateinit var imageView: ImageView
-    private lateinit var selectedImageUri: Uri
+    private lateinit var ivProfile: CircleImageView
+    private var selectedImageUri: Uri ?= null
     private lateinit var storageReference: StorageReference
 
     private lateinit var binding: ActivityCreateProfileBinding
@@ -32,14 +35,32 @@ class CreateProfileActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val ivProfile: CircleImageView = binding.ivProfile
+        ivProfile = binding.ivProfile
 
         val storage = Firebase.storage
         storageReference = storage.reference
+
+//        val tvUpload: TextView = binding.tvUpload
+//        tvUpload.setOnClickListener {
+//            onImageViewClick()
+//        }
     }
 
     fun onImageViewClick(view: View) {
+//        lateinit var galleryIntent: Intent
+//        galleryIntent.setAction(Intent.ACTION_GET_CONTENT)
+//        galleryIntent.setType("image/*")
+//        startActivityForResult(galleryIntent, 2)
+        Log.d("tvUpload", "onImageViewClick: clicked")
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            selectedImageUri = data.data
+            ivProfile.setImageURI(selectedImageUri)
+        }
     }
 
     private fun openImagePicker() {
